b9824a307fd09388b6e9132d1402602a1de836bd
Sinchu25 <108563647+Sinchu25@users.noreply.github.com>
2025-01-01 20:52:41 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -21,3 +21,22 @@ const Counter = () => {
 };
 
 export default Counter;
+
+
+app.js
+
+
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
+
